a0547bee1039e509996094a4df85e8d7f337ecb3
Leonardo <leonardo.pacciulli@opah.com.br>
2021-09-14 19:42:06 -0300

    Creating pthe project, and some components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+export const App = () => {
+	return (
+		<>
+			<Header />
+			<Dashboard />
+			<GlobalStyle />
+		</>
+	);
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import income from '../../assets/income.svg'
+import outcome from '../../assets/outcome.svg'
+import total from '../../assets/total.svg'
+
+import { Container } from './styles'
+
+export const Summary = () => {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={income} alt="Entradas" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcome} alt="Saídas" />
+        </header>
+        <strong>-R$ 500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={total} alt="Total" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  )
+}
